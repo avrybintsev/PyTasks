9ad7d22c00c6349ba9531851c2d082bf2c3f7dc4
avrybintsev <avrybintsev@gmail.com>
2013-01-29 00:31:41 +0400

    task5 changes

--- task5.py
+++ task5.py
@@ -1,1 +1,1 @@
-eratosthenes = lambda k: filter( lambda x: x not in [n*q for q in xrange(2,int(k ** 0.5 + 1)+1) for n in xrange(2,k // q + 1)], xrange(2,k+1) )
+eratosthenes = lambda k: filter( lambda x: x not in [n*q for q in xrange(2,int(k ** 0.5 + 1)+1) for n in xrange(2, k//q+1)], xrange(2,k+1) )
